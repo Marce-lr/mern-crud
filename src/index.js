4cecc8b6203f195660c7768ad00cc7ed1445a97a
Andrea <marceitha123@gmail.com>
2025-01-31 18:28:17 -0600

    Actualizar URL del backend en el frontend

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 import { connectDB } from "./db.js";
 
-const puerto = process.env.PORT || 4000;
+const puerto = process.env.PORT || 5000;
 connectDB();
 app.listen(puerto);
 console.log("Server on port", puerto);
